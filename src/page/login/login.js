d6c2662b09ff27198c664ff67c4ae98a6cf2231b
Alvin Acosta <alvin.acosta@movon.com.ph>
2021-10-15 17:45:10 +0800

    add and style input fields and login button

--- src/page/login/login.js
+++ src/page/login/login.js
@@ -2,7 +2,8 @@ import React from "react";
 import "./login.css";
 import bg from "../../assets/images/loginBg.jpg";
 import phone from "../../assets/images/Phone.png";
-import { Image, Form, Input, Button } from "antd";
+import { Image, Form, Input, Button, Radio } from "antd";
+import { PhoneOutlined, LockOutlined } from "@ant-design/icons";
 
 function Login() {
   return (
@@ -10,9 +11,17 @@ function Login() {
       <Image src={bg} className="loginPage__bgImage" />
       <div className="loginPage__formContainer">
         <Form className="loginPage__form">
-          <Input />
-          <Input />
-          <Button>CLICK ME</Button>
+          <span className="loginPage__inputSpan">
+            <PhoneOutlined style={{ fontSize: "20px", color: "#2e418c" }} />{" "}
+            <Input placeholder="Phone Number" className="loginPage__input" />
+          </span>
+          <span className="loginPage__inputSpan">
+            <LockOutlined style={{ fontSize: "20px", color: "#2e418c" }} />{" "}
+            <Input placeholder="Password" className="loginPage__input" />
+          </span>
+
+          <Button className="loginPage__button">Login</Button>
+          <Radio className="loginPage__radio"> Remember</Radio>
         </Form>
       </div>
     </div>
