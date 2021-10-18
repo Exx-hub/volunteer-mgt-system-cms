a6d61356eab8b2cb4691e9c48a1d132573629844
Alvin Acosta <alvin.acosta@movon.com.ph>
2021-10-18 15:30:46 +0800

    adjust input styles, autofocus mobile input, add spinner, add input state

--- src/page/login/login.js
+++ src/page/login/login.js
@@ -1,26 +1,89 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./login.css";
 import bg from "../../assets/images/loginBg.jpg";
 import phone from "../../assets/images/Phone.png";
-import { Image, Form, Input, Button, Radio } from "antd";
-import { PhoneOutlined, LockOutlined } from "@ant-design/icons";
+import { Image, Form, Input, Button, Radio, Spin } from "antd";
+import {
+  PhoneOutlined,
+  LockOutlined,
+  LoadingOutlined,
+} from "@ant-design/icons";
 
 function Login() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [mobile, setMobile] = useState("");
+  const [password, setPassword] = useState("");
+
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  const onFinish = () => {
+    setIsLoading(true);
+
+    // LOGIN API CALL HERE
+
+    setTimeout(() => {
+      setIsLoading(false);
+      // alert("login clicked");
+      console.log("mobile:", mobile);
+      console.log("password", password);
+
+      setMobile("");
+      setPassword("");
+    }, 2000);
+  };
+
   return (
     <div className="loginPage">
       <Image src={bg} className="loginPage__bgImage" />
       <div className="loginPage__formContainer">
-        <Form className="loginPage__form">
+        <Form onFinish={onFinish} className="loginPage__form">
           <span className="loginPage__inputSpan">
             <PhoneOutlined style={{ fontSize: "20px", color: "#2e418c" }} />{" "}
-            <Input placeholder="Phone Number" className="loginPage__input" />
+            <Input
+              placeholder="Phone Number"
+              className="loginPage__input"
+              type="number"
+              ref={inputRef}
+              value={mobile}
+              onChange={(e) => setMobile(e.target.value)}
+            />
           </span>
           <span className="loginPage__inputSpan">
             <LockOutlined style={{ fontSize: "20px", color: "#2e418c" }} />{" "}
-            <Input placeholder="Password" className="loginPage__input" />
+            <Input
+              type="password"
+              placeholder="Password"
+              className="loginPage__input"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </span>
 
-          <Button className="loginPage__button">Login</Button>
+          <Button
+            htmlType="submit"
+            disabled={isLoading}
+            className={
+              isLoading ? "loginPage__button disabled" : "loginPage__button"
+            }
+          >
+            {isLoading && (
+              <div className="loginPage__button--spinner">
+                <Spin
+                  indicator={
+                    <LoadingOutlined
+                      style={{ fontSize: 24, color: "lightgray" }}
+                      spin
+                    />
+                  }
+                />
+              </div>
+            )}
+            Login
+          </Button>
           <Radio className="loginPage__radio"> Remember</Radio>
         </Form>
       </div>
