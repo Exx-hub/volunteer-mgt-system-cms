b11cc4175530561092d751ce1359e7fc494c5877
Alvin Acosta <alvin.acosta@movon.com.ph>
2021-10-19 14:49:18 +0800

    navigation feature and highlight menu when page active

---
+++ src/page/bulletin/bulletin.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Bulletin() {
+  return (
+    <div>
+      <h1>BULLETIN MANAGEMENT</h1>
+    </div>
+  );
+}
+
+export default Bulletin;
--- src/page/home/home.js
+++ src/page/home/home.js
@@ -1,26 +1,70 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./home.css";
 import "../../assets/images/sidebar.png";
 import SideBar from "../../components/SideBar";
-import { Switch, Route } from "react-router-dom";
+import {
+  Switch,
+  Route,
+  Redirect,
+  useHistory,
+  useLocation,
+} from "react-router-dom";
 
 import { Layout, Menu } from "antd";
 
 import Users from "../user";
 import Regional from "../regional";
+import Bulletin from "../bulletin";
+import About from "../about";
 
 const { Content, Sider } = Layout;
 const { SubMenu } = Menu;
 
 function Home() {
+  const history = useHistory();
+  const location = useLocation();
+
+  const [active, setActive] = useState("");
+
+  useEffect(() => {
+    if (location.pathname === "/home/users") setActive("users");
+    if (location.pathname === "/home/regional") setActive("regional");
+    if (location.pathname === "/home/bulletin") setActive("bulletin");
+    if (location.pathname === "/home/about") setActive("about");
+  }, [location.pathname]);
+
   return (
     <Layout className="homePage">
       <Sider width={280} className="homePage__sider">
         <Menu className="homePage__sider--menu" mode="inline">
-          <Menu.Item className="menu__item">Users</Menu.Item>
-          <Menu.Item className="menu__item">Regional Management</Menu.Item>
-          <Menu.Item className="menu__item">Bulletin Management</Menu.Item>
-          <Menu.Item className="menu__item">About</Menu.Item>
+          <Menu.Item
+            className={active === "users" ? "menu__item active" : "menu__item"}
+            onClick={() => history.push("/home/users")}
+          >
+            Users
+          </Menu.Item>
+          <Menu.Item
+            className={
+              active === "regional" ? "menu__item active" : "menu__item"
+            }
+            onClick={() => history.push("/home/regional")}
+          >
+            Regional Management
+          </Menu.Item>
+          <Menu.Item
+            className={
+              active === "bulletin" ? "menu__item active" : "menu__item"
+            }
+            onClick={() => history.push("/home/bulletin")}
+          >
+            Bulletin Management
+          </Menu.Item>
+          <Menu.Item
+            className={active === "about" ? "menu__item active" : "menu__item"}
+            onClick={() => history.push("/home/about")}
+          >
+            About
+          </Menu.Item>
         </Menu>
       </Sider>
 
@@ -33,6 +77,13 @@ function Home() {
             <Route path="/home/regional">
               <Regional />
             </Route>
+            <Route path="/home/bulletin">
+              <Bulletin />
+            </Route>
+            <Route path="/home/about">
+              <About />
+            </Route>
+            <Redirect from="/home" to="/home/users" />
           </Switch>
         </Content>
       </Layout>
